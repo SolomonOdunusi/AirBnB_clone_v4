f1398af3a6769be6102eb7039056712ca0fb7bcf
SolomonOdunusi <solomonodunusi@gmail.com>
2024-01-13 09:55:41 -0800

    Added python scripts for the routes

---
+++ web_dynamic/2-hbnb.py
@@ -0,0 +1,33 @@
+"""Create a Flask app
+that listens on port 5000"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from uuid import uuid4
+
+app = Flask(__name__)
+
+
+@app.route('/2-hbnb/', strict_slashes=False)
+def hbnb():
+    """Display a HTML page"""
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    places = storage.all(Place).values()
+    return render_template('100-hbnb.html',
+                           states=states,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=uuid4())
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the current db session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
